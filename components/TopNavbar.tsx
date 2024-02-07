6216e31eec504c60cc3e9ae18cf3ff068dc631d7
Muhammad Nur Faza <gofar222@gmail.com>
2024-02-07 14:41:08 +0700

    add download and apk

--- components/TopNavbar.tsx
+++ components/TopNavbar.tsx
@@ -6,9 +6,15 @@ import React, { useState } from "react";
 import { MainNav } from "./NavbarMenuComp";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import useDownloader from "react-use-downloader";
 
 function TopNavbar() {
   const [activeTab, setActiveTab] = useState("");
+
+  const { download } = useDownloader();
+
+  const fileUrl = "/apk/PSA.apk";
+  const filename = "PSA.apk";
 
   return (
     <div>
@@ -29,40 +35,53 @@ function TopNavbar() {
             activeTab={activeTab}
             setActiveTab={setActiveTab}
           />
-          <div className="hidden max-lg:inline">
-            <Sheet>
-              <SheetTrigger className="border border-white/50 p-2 rounded-md">
-                <Menu color="white" />
-              </SheetTrigger>
-              <SheetContent side={"top"} className="bg-slate-600">
-                <div className="flex flex-col gap-10 mt-6 text-center">
-                  <Link
-                    href="/ongoing"
-                    className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
-                  >
-                    Sedang Tayang
-                  </Link>
-                  <Link
-                    href="/popular"
-                    className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
-                  >
-                    Populer
-                  </Link>
-                  <Link
-                    href="/upcoming"
-                    className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
-                  >
-                    Mendatang
-                  </Link>
-                  <Link
-                    href="/search"
-                    className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
-                  >
-                    Pencarian
-                  </Link>
-                </div>
-              </SheetContent>
-            </Sheet>
+          <div className="hidden max-lg:flex max-lg:gap-4">
+            <div
+              className="bg-red-600 p-2 rounded-md cursor-pointer h-[42px] w-[42px]"
+              onClick={() => download(fileUrl, filename)}
+            >
+              <Image
+                src="/icons/icons-download.png"
+                alt="download"
+                height={24}
+                width={24}
+              ></Image>
+            </div>
+            <div>
+              <Sheet>
+                <SheetTrigger className="border border-white/50 p-2 rounded-md">
+                  <Menu color="white" />
+                </SheetTrigger>
+                <SheetContent side={"top"} className="bg-slate-600">
+                  <div className="flex flex-col gap-10 mt-6 text-center">
+                    <Link
+                      href="/ongoing"
+                      className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
+                    >
+                      Sedang Tayang
+                    </Link>
+                    <Link
+                      href="/popular"
+                      className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
+                    >
+                      Populer
+                    </Link>
+                    <Link
+                      href="/upcoming"
+                      className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
+                    >
+                      Mendatang
+                    </Link>
+                    <Link
+                      href="/search"
+                      className="text-md font-light text-slate-200 transition-colors hover:text-red-600"
+                    >
+                      Pencarian
+                    </Link>
+                  </div>
+                </SheetContent>
+              </Sheet>
+            </div>
           </div>
         </div>
         <div className="w-60 max-lg:hidden border-b border-b-slate-500 flex px-1 py-3 gap-3">
