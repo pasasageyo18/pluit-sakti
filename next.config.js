6b232580bdf40b938f47678eaaa9a002e2d1aa3d
Muhammad Nur Faza <gofar222@gmail.com>
2024-02-07 10:40:40 +0700

    adding pwa config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public", // Destination directory for the PWA files
+  cacheOnFrontEndNav: true,
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true
+  }
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
